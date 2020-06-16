bc8f6f9c9fdd549ba68385ba2d0aaacc2a523465
JoseOgam <jotienoochieng@gmail.com>
2020-06-16 13:24:56 +0300

    node argument vector

--- notes.js
+++ notes.js
@@ -3,8 +3,13 @@ var getNotes = function() {
     return "concerts are fan";
 }
 
-var addNote = function(title, body) {}
-var saveNotes = function(notes) {}
+var addNote = function(title, body) {
+    var notes = laodNote();
+}
+var saveNotes = function(notes) {
+    var dataJSON = JSON.stringify(notes);
+    fs.writeFileSync("notes.json", dataJSON);
+}
 var laodNote = function() {
     try {
         var data = fs.readFileSync(notes.json);
